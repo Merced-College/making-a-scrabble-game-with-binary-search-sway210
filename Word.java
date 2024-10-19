8a8da69ef5ba59ea7a5f94a1b86c5ceb309d2981
Jose Arellano <157650048+sway210@users.noreply.github.com>
2024-10-18 21:20:15 -0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,4 +1,4 @@
-package scrabbleGame;
+//Luis: word.java
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +16,7 @@ public class Word implements Comparable<Word> {
     private static final ArrayList<Integer> letterScores = new ArrayList<>(Arrays.asList(
         1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10));
 
-    // Constructor
+    // Ethan
     public Word(String word) {
         this.word = word.toUpperCase(); // Store words in uppercase
         score = calculateScore(); // Set score to be calculated amount
@@ -33,20 +33,20 @@ public class Word implements Comparable<Word> {
         }
         return score; 
     }
-
+    //Luis
     public String getWord() {
         return word;
     }
-
+    //Luis
     public int getScore() {
         return score; // Return score of the word
     }
-
+    //Luis
     @Override
     public int compareTo(Word other) {
         return this.word.compareTo(other.word); // Compare based on the word string
     }
-
+    //Luis
     @Override
     public String toString() {
         return word; // Return just the word when printing
